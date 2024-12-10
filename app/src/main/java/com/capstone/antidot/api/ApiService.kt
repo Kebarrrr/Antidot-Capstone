4a0e13756e82d179918f8cac2e357e294a8319a5
mreynaldo <muhammadreynaldoauliarachman@gmail.com>
2024-12-10 22:51:26 +0700

    add Item Reminder.xml

--- app/src/main/java/com/capstone/antidot/api/ApiService.kt
+++ app/src/main/java/com/capstone/antidot/api/ApiService.kt
@@ -11,7 +11,6 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface ApiService {
     @POST("auth/login")
